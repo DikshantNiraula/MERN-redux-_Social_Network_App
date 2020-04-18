23dae9267becb38e5283150b861741313dabf952
DikshantNiraula <dikshantniraula351@gmail.com>
2020-04-18 13:40:39 +0545

    all profile action made

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -3,7 +3,7 @@ import { setAlert } from "./alert";
 
 import {
   GET_PROFILE,
-  // GET_PROFILES,
+  GET_PROFILES,
   PROFILE_ERROR,
   UPDATE_PROFILE,
   CLEAR_PROFILE,
@@ -28,40 +28,40 @@ export const getCurrentProfile = () => async (dispatch) => {
 };
 
 // Get all profiles
-// export const getProfiles = () => async dispatch => {
-//   dispatch({ type: CLEAR_PROFILE });
+export const getProfiles = () => async (dispatch) => {
+  dispatch({ type: CLEAR_PROFILE });
 
-//   try {
-//     const res = await axios.get('/api/profile');
+  try {
+    const res = await axios.get("/api/profile");
 
-//     dispatch({
-//       type: GET_PROFILES,
-//       payload: res.data
-//     });
-//   } catch (err) {
-//     dispatch({
-//       type: PROFILE_ERROR,
-//       payload: { msg: err.response.statusText, status: err.response.status }
-//     });
-//   }
-// };
+    dispatch({
+      type: GET_PROFILES,
+      payload: res.data,
+    });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
 
 // Get profile by ID
-// export const getProfileById = userId => async dispatch => {
-//   try {
-//     const res = await axios.get(`/api/profile/user/${userId}`);
+export const getProfileById = (userId) => async (dispatch) => {
+  try {
+    const res = await axios.get(`/api/profile/user/${userId}`);
 
-//     dispatch({
-//       type: GET_PROFILE,
-//       payload: res.data
-//     });
-//   } catch (err) {
-//     dispatch({
-//       type: PROFILE_ERROR,
-//       payload: { msg: err.response.statusText, status: err.response.status }
-//     });
-//   }
-// };
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data,
+    });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
 
 // Create or update profile
 export const createProfile = (formData, history, edit = false) => async (
--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -20,6 +20,7 @@ export const CLEAR_PROFILE = "CLEAR_PROFILE";
 
 //Profile
 export const GET_PROFILE = "GET_PROFILE";
+export const GET_PROFILES = "GET_PROFILES";
 export const PROFILE_ERROR = "PROFILE_ERROR";
 
 //Update Profile
